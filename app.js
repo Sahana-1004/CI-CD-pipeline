fa6f69bdd6aa743ba1213ee598c46e3024a4b76d
Sahana_10 <loveinshadows2000@gmail.com>
2025-11-20 16:57:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! This is my CI/CD pipeline running on Google Cloud Run!');
+  res.send('Hello, World! This is my CI/CD pipeline running on Google Cloud walk!');
 });
 
 app.listen(PORT, () => {
   console.log(Server listening on port ${PORT}...);
+
 });
